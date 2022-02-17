6b32daf1dd080bbe2a05a8ae81077ce20037568c
seongjun kim <tjdwns5063@naver.com>
2022-02-17 16:38:49 +0900

    refactor cpp01/ex03

--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,16 +6,21 @@ int main()
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
+		std::cout << '\n';
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
+		std::cout << '\n';
 	}
 	{
+		std::cout << '\n';
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
+		std::cout << '\n';
 		jim.setWeapon(&club);
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
+		std::cout << '\n';
 	}
 }
