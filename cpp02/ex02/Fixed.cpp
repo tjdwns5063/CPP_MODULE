ffcca94562443cf3589e754d8303691a01e0f15c
seongjun kim <tjdwns5063@naver.com>
2022-02-23 20:41:06 +0900

    impl cpp02/ex02

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,147 @@
+#include "Fixed.hpp"
+#include <cmath>
+
+const int Fixed::fractionalBits = 8;
+
+Fixed::Fixed( void ) {
+    rawBits = 0;
+    //std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed( int rawBit ) {
+    rawBits = rawBit << fractionalBits;
+    //std::cout << "Int constructor called\n";
+}
+Fixed::Fixed( float rawBit ) {
+    rawBits = roundf(rawBit * (1 << fractionalBits));
+    //std::cout << "Float constructor called\n";
+}
+
+Fixed::Fixed( const Fixed& ref ) {
+    //std::cout << "Copy constructor called\n";
+    *this = ref;
+}
+
+Fixed& Fixed::operator=( const Fixed& ref ) {
+    //std::cout << "Assignation operator called\n";
+    rawBits = ref.getRawBits();
+    return (*this);
+}
+
+Fixed::~Fixed( void ) {
+    //std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits( void ) const {
+    return (rawBits);
+}
+
+void    Fixed::setRawBits( int const raw ) {
+    rawBits = raw;
+}
+
+float   Fixed::toFloat( void ) const {
+    float   ret;
+
+    ret = static_cast<float>(rawBits) / (1 << fractionalBits);
+    return (ret);
+}
+
+int     Fixed::toInt( void ) const {
+    int ret;
+
+    ret = this->getRawBits() >> fractionalBits;
+    return (ret);
+}
+
+std::ostream&   operator<<( std::ostream& os, const Fixed& ref ) {
+    std::cout << ref.toFloat();
+    return (os);
+}
+
+bool    Fixed::operator>( const Fixed& next) const {
+    return (rawBits > next.rawBits);
+}
+
+bool    Fixed::operator<( const Fixed& next ) const {
+    return (rawBits < next.rawBits);
+}
+
+bool    Fixed::operator>=( const Fixed& next ) const {
+    return (rawBits >= next.rawBits);
+}
+
+bool    Fixed::operator==( const Fixed& next ) const {
+    return (rawBits == next.rawBits);
+}
+
+bool    Fixed::operator<=( const Fixed& next ) const {
+    return (rawBits <= next.rawBits);
+}
+
+bool    Fixed::operator!=( const Fixed& next ) const {
+    return (rawBits != next.rawBits);
+}
+
+Fixed   Fixed::operator+( const Fixed& next ) const {
+    Fixed   temp(this->toFloat() + next.toFloat());
+    return (temp);
+}
+
+Fixed   Fixed::operator-( const Fixed& next ) const {
+    Fixed   temp(this->toFloat() - next.toFloat());
+    return (temp);
+}
+
+Fixed   Fixed::operator*( const Fixed& next ) const {
+    Fixed   temp(this->toFloat() * next.toFloat());
+    return (temp);
+}
+
+Fixed   Fixed::operator/( const Fixed& next ) const {
+    if (next.getRawBits() == 0) {
+        std::cout << "division by zero is undefined!!!\n";
+    }
+    Fixed   temp(this->toFloat() / next.toFloat());
+    return (temp);
+}
+
+Fixed&   Fixed::operator++( void ) {
+    ++rawBits;
+    return (*this);
+}
+
+Fixed   Fixed::operator++( int ) {
+    Fixed   temp = *this;
+
+    rawBits++;
+    return (temp);
+}
+
+Fixed&	Fixed::operator--( void ) {
+	--rawBits;
+	return (*this);
+}
+
+Fixed	Fixed::operator--( int ) {
+	Fixed	temp = *this;
+
+	rawBits--;
+	return (temp);
+}
+
+Fixed&   Fixed::max( const Fixed& prev, const Fixed& next ) {
+	if (prev >= next) {
+		return (const_cast<Fixed&>(prev));
+	} else {
+		return (const_cast<Fixed&>(next));
+	}
+}
+
+Fixed&	Fixed::min( const Fixed& prev, const Fixed& next ) {
+	if (prev <= next) {
+		return (const_cast<Fixed&>(prev));
+	} else {
+		return (const_cast<Fixed&>(next));
+	}
+}
